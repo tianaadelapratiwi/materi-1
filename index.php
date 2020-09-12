8ae29c8504457055937219771178dab295543e12
tianaadelapratiwi <71165318+tianaadelapratiwi@users.noreply.github.com>
2020-09-12 20:45:40 +0700

    PHP Object Oriented : Inheritence

--- index.php
+++ index.php
@@ -13,24 +13,28 @@ class Mobil {
 	}
 }
 
-$bmw = new mobil;
+class BMW extends Mobil {
+
+}
+
+class Tesla extends Mobil {
+	function selfParking (){
+		echo "Parkir Sendiri";
+	}
+}
+
+
+$bmw = new BMW;
 $bmw->merk = "BMW";
 $bmw->tipe = "320i";
 $bmw->mesin = "2000cc";
 $bmw->max_speed = "280km/h";
 
-$bmw2 = new mobil;
-$bmw2->merk = "BMW";
-$bmw2->tipe = "320i";
-$bmw2->mesin = "2000cc";
-$bmw2->max_speed = "280km/h";
+$tesla = new Tesla;
+$tesla->merk = "Tesla";
+$tesla->tipe = "Model X";
+$tesla->mesin = "Listrik";
+$tesla->max_speed = "280km/h";
 
-echo "$bmw2->tipe";
-echo "<br>";
-echo "$bmw->tipe";
 
-if($bmw === $bmw2){
-	echo "Sama";
-}else{
-	echo "Tidak";
-}
+echo $tesla->kecepatanMaksimal();
