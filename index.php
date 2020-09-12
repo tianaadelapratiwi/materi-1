1f2a1af0ed83bc43afb720b3e7eaa63c4ba79a8d
tianaadelapratiwi <71165318+tianaadelapratiwi@users.noreply.github.com>
2020-09-12 19:52:21 +0700

    membuat index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+class Mobil {
+
+	public $merk, $tipe, $mesin, $max_speed;
+
+	public function cetakTipe(){
+		return $this->tipe;
+	}
+}
+
+$bmw = new mobil;
+$bmw->merk = "BMW";
+$bmw->tipe = "320i";
+$bmw->mesin = "2000cc";
+$bmw->max_speed = "280km/h";
+
+encho "Tipe ".$bmw->cetakTipe();
